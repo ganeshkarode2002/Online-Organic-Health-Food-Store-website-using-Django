67039ce678ac676da9612a615afc758e3fd8ba5f
ganesh <ganesh.karode20@vit.edu>
2022-12-28 09:31:03 +0530

    Initial commit

---
+++ store/urls.py
@@ -0,0 +1,35 @@
+"""Eshop URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import path
+from .views.home import Index
+from .views.login import Login , logout
+from .views.signup import Signup
+from .views.cart import Cart
+from .views.checkout import Checkout
+from .views.orders import OrderView
+from .middlewares.auth import auth_middleware
+
+urlpatterns = [
+    path('', Index.as_view(), name='homepage'),
+    path('signup', Signup.as_view(), name='signup'),
+    path('login', Login.as_view(), name='login'),
+    path('logout', logout, name='logout'),
+    path('cart', Cart.as_view(), name='cart'),
+    path('check-out', Checkout.as_view(), name='checkout'),
+    path('orders', auth_middleware(OrderView.as_view()), name='orders'),
+
+]
